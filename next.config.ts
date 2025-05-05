46ba413f03b0fc5362686ebb94c31c0cbc2b5057
Tom Donath <Tom.Donath@pta.de>
2025-05-05 20:51:43 +0200

    feat(general): :sparkles: init project setup
    install next, pwa, mui, supabase and configure

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+};
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
+export default nextConfig;
